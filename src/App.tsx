58c8fdec4b6bab42fd95f69128dedbdd7bdfdadd
Udana Nimsara <nimsaragcu.22@uom.lk>
2024-06-06 01:40:44 +0530

    wanna see how this works in tma

--- src/App.tsx
+++ src/App.tsx
@@ -37,44 +37,66 @@ export default function App() {
 
   // Preview of Website
   return (
-    <div className="min-h-full flex flex-col items-center justify-center font-['Poppins'] bg-gradient-to-r from-violet-600 to-indigo-600">
-      <div className='p-5 max-w-[500px] flex flex-col gap-[30px] items-center mx-auto text-center'>
+    <div className="min-h-screen flex flex-col items-center justify-center font-['Poppins'] bg-gradient-to-r from-violet-600 to-indigo-600">
+      <div className='p-5 max-w-[500px] w-full flex flex-col gap-[30px] items-center mx-auto text-center'>
+      <TonConnectButton />
         <div className="text-2xl font-black">CentiXDoge</div>
         <img width="50%" height="auto" src="dog.png" alt="Logo"/>
       </div>
       <div>
-      <div className='flex items-center justify-center flex-col w-full p-2.5 px-5 rounded-lg bg-white'>
+      <div className='flex items-center justify-center flex-col w-full md:w-[10px] p-2.5 px-5 rounded-[40px] bg-white'>
           <b className=" text-gray-500">Account Balance</b>
           {contract_balance && (
             <div className="text-4xl m-3 font-bold">{fromNano(contract_balance)}</div>
           )}
-          <div className="flex justify-center justify-items-center">
-          <div className = "pr-2"><img height="35" width="35" src="https://cdn.simpleicons.org/ton"/></div> 
-          <div className="text-[25px] text-gray-500 font-medium">TON</div>
-          </div>
-        </div>
-        <div className='w-full h-[120px] bg-blue-100 rounded-[20px] p-[10px] my-[10px]'>
-        <div className="flex justify-center">
-        <div className="mr-[50px] mt-[10px]"><img height="55" width="55" src="topup.png"/></div>
-        <div className="ml-[50px] mt-[10px]"><img height="55" width="55" src="withdraw.png"/></div>
-        </div >
-        <div className="flex justify-center">
-        <div className="pr-[10px] pl-[25px] mt-[10px]"> TOP UP </div>
-        <div className="ml-[80px] pr-2 mt-[10px]"> WITHDRAW </div>
-        </div>
-        </div>
-        <div className="text-red-900 text-xl">
-          <b>{WebApp.platform}</b>
-          <b>Our contract Address</b>
-          <div className="text-red-900">{contract_address?.slice(0, 30) + "..."}</div>
-        </div>
-        <div>
+          <div className="mb-3">
           <button onClick={copyToClipboard}>
           {contract_address?.slice(0, 30) + "..."}
           </button>
           {copySuccess && <div style={{ color: 'green' }}>{copySuccess}</div>}
         </div>
-        <TonConnectButton />
+          <div className="flex justify-center justify-items-center">
+          <div className = "pr-2"><img height="35" width="35" src="https://cdn.simpleicons.org/ton"/></div> 
+          <div className="text-[25px] text-gray-500 font-medium">TON</div>
+          </div>
+        </div>
+        <div className='w-full h-[120px] bg-blue-100 rounded-[30px] p-[5px] my-[10px]'>
+        <div className="flex justify-center">
+        <div className="mr-[40px]">
+        {connected && (
+          <a
+          className={`Button ${connected ? 'Active' : 'Disabled'}`}
+            onClick={() => {
+              sendDeposit();
+            }}
+          >
+          <img height="55" width="55" src="topup.png"/>
+          </a>
+        )}
+          </div>
+        <div className="ml-[50px]">
+        {connected && (
+          <a
+          className={`Button ${connected ? 'Active' : 'Disabled'}`}
+            onClick={() => {
+              sendWithdrawalRequest();
+            }}
+          >
+          <img height="55" width="55" src="withdraw.png"/>
+          </a>
+        )}
+        </div>
+        </div >
+        <div className="flex justify-center">
+        <div className="pr-[20px] pl-[25px]"> TOP UP </div>
+        <div className="ml-[90px] pr-2"> WITHDRAW </div>
+        </div>
+        </div>
+        <div className="text-red-900 text-xl">
+          <b>{WebApp.platform}</b>
+        </div>
+        
+        
         
 
         <div className='Card'>
